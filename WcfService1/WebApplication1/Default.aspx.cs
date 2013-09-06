73006831a71ab3ba0d888ba86a7ce64b7a066af1
Jose Diaz <jamdiazdiaz@gmail.com>
2013-09-05 20:47:46 -0500

    Incluyendo proyecto servicio

---
+++ WcfService1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WebApplication1.ServiceReference1;
+
+namespace WebApplication1
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            IService1 servicio = new Service1Client();
+
+            var lista = servicio.ObtenerCategorias();
+
+            GridView1.DataSource = lista;
+            GridView1.DataBind();
+
+        }
+    }
+}
